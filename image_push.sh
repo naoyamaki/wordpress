8d4b435d1419891d17f0a7e1eb2d0280df3fbd1f
naoyamaki <stringbassandsticks@gmail.com>
2021-06-20 22:02:44 +0900

    もろもろチューニング
    ・web->app間のホスト指定を環境変数に移行
    ・ECRにpushするシェルに元イメージ削除、ログイン認証を追加
    ・各イメージのtimezone修正

--- image_push.sh
+++ image_push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+docker-compose down --rmi all --volumes --remove-orphans
 docker compose build
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin https://247480313130.dkr.ecr.ap-northeast-1.amazonaws.com
 docker tag wp_web:latest 247480313130.dkr.ecr.ap-northeast-1.amazonaws.com/wp_web:latest
 docker push 247480313130.dkr.ecr.ap-northeast-1.amazonaws.com/wp_web:latest
 docker tag wp_app:latest 247480313130.dkr.ecr.ap-northeast-1.amazonaws.com/wp_app:latest

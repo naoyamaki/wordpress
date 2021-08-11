3f77bedea6375e82d95ee0865e139f9c2a86d07d
nao.yamaki <stringbassandsticks@gmail.com>
2021-08-11 14:26:27 +0900

    terraformの残エラー対応

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,82 +1,82 @@
-variable ecr_web_image {}
-variable ecr_app_image {}
-variable db_endpoint {}
-variable db_password {}
+data "aws_caller_identity" "self" {}
+variable "db_endpoint" {}
+variable "db_password" {}
 
-provider "aws"{
-	region="ap-northeast-1"
+provider "aws" {
+  region = "ap-northeast-1"
 }
+
 # VPC/network
 resource "aws_vpc" "wordpress_vpc" {
-    cidr_block = "10.1.0.0/16"
-    instance_tenancy = "default"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "true"
-    tags = {
-      Name = "wordpress_vpc"
-    }
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "wordpress_vpc"
+  }
 }
 resource "aws_internet_gateway" "wordpress_igw" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
 }
 
 resource "aws_subnet" "public-a" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
-    cidr_block = "10.1.1.0/24"
-    availability_zone = "ap-northeast-1a"
-    tags = {
-      Name = "wordpress_pubsub_1a"
-    }
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "wordpress_pubsub_1a"
+  }
 }
 resource "aws_subnet" "public-c" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
-    cidr_block = "10.1.2.0/24"
-    availability_zone = "ap-northeast-1c"
-    tags = {
-      Name = "wordpress_pubsub_1c"
-    }
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  cidr_block        = "10.1.2.0/24"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "wordpress_pubsub_1c"
+  }
 }
 resource "aws_subnet" "private-a" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
-    cidr_block = "10.1.128.0/24"
-    availability_zone = "ap-northeast-1a"
-    tags = {
-      Name = "wordpress_pvtsub_1a"
-    }
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  cidr_block        = "10.1.128.0/24"
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "wordpress_pvtsub_1a"
+  }
 }
 resource "aws_subnet" "private-c" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
-    cidr_block = "10.1.129.0/24"
-    availability_zone = "ap-northeast-1c"
-    tags = {
-      Name = "wordpress_pvtsub_1c"
-    }
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  cidr_block        = "10.1.129.0/24"
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "wordpress_pvtsub_1c"
+  }
 }
 
 resource "aws_route_table" "public-route" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.wordpress_igw.id}"
-    }
+  vpc_id = aws_vpc.wordpress_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.wordpress_igw.id
+  }
 }
 resource "aws_route_table" "private-route" {
-    vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
 }
 
 resource "aws_route_table_association" "puclic-a" {
-    subnet_id = "${aws_subnet.public-a.id}"
-    route_table_id = "${aws_route_table.public-route.id}"
+  subnet_id      = aws_subnet.public-a.id
+  route_table_id = aws_route_table.public-route.id
 }
 resource "aws_route_table_association" "puclic-c" {
-    subnet_id = "${aws_subnet.public-c.id}"
-    route_table_id = "${aws_route_table.public-route.id}"
+  subnet_id      = aws_subnet.public-c.id
+  route_table_id = aws_route_table.public-route.id
 }
 
 # security group
 resource "aws_security_group" "wordpress-alb-sg" {
   name   = "wordpress-alb-sg"
-  vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
 
   egress {
     from_port   = 0
@@ -94,7 +94,7 @@ resource "aws_security_group" "wordpress-alb-sg" {
 }
 resource "aws_security_group" "wordpress-service-sg" {
   name   = "wordpress-service-sg"
-  vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -102,15 +102,15 @@ resource "aws_security_group" "wordpress-service-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.wordpress-alb-sg.id}"]
   }
 }
 resource "aws_security_group" "wordpress-efs-sg" {
   name   = "wordpress-efs-sg"
-  vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -118,15 +118,15 @@ resource "aws_security_group" "wordpress-efs-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.wordpress-service-sg.id}"]
   }
 }
 resource "aws_security_group" "wordpress-rds-sg" {
   name   = "wordpress-rds-sg"
-  vpc_id = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id = aws_vpc.wordpress_vpc.id
   egress {
     from_port   = 0
     to_port     = 0
@@ -134,9 +134,9 @@ resource "aws_security_group" "wordpress-rds-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.wordpress-service-sg.id}"]
   }
   ingress {
@@ -177,7 +177,7 @@ resource "aws_lb_target_group" "wordpress-tgg" {
   }
 
   target_type = "ip"
-  vpc_id      = "${aws_vpc.wordpress_vpc.id}"
+  vpc_id      = aws_vpc.wordpress_vpc.id
 }
 resource "aws_lb" "wordpress-alb" {
   drop_invalid_header_fields = "false"
@@ -189,15 +189,15 @@ resource "aws_lb" "wordpress-alb" {
   load_balancer_type         = "application"
   name                       = "wordpress-alb"
   security_groups            = ["${aws_security_group.wordpress-alb-sg.id}"]
-  subnets = ["${aws_subnet.public-a.id}", "${aws_subnet.public-c.id}"]
+  subnets                    = ["${aws_subnet.public-a.id}", "${aws_subnet.public-c.id}"]
 }
 resource "aws_lb_listener" "wordpress-alb-listner" {
-  load_balancer_arn = "${aws_lb.wordpress-alb.arn}"
+  load_balancer_arn = aws_lb.wordpress-alb.arn
   port              = 443
   protocol          = "TCP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.wordpress-tgg.arn}"
+    target_group_arn = aws_lb_target_group.wordpress-tgg.arn
     type             = "forward"
   }
 }
@@ -213,84 +213,80 @@ resource "aws_efs_file_system" "wordpress-efs" {
   throughput_mode = "bursting"
 }
 resource "aws_efs_mount_target" "wordpress-efs-tgt-1a" {
-  file_system_id  = "${aws_efs_file_system.wordpress-efs.id}"
-  security_groups = "${aws_security_group.wordpress-efs-sg.id}"
-  subnet_id       = "${aws_subnet.public-a.id}"
+  file_system_id = aws_efs_file_system.wordpress-efs.id
+  subnet_id      = aws_subnet.public-a.id
 }
 resource "aws_efs_mount_target" "wordpress-efs-tgt-1c" {
-  file_system_id  = "${aws_efs_file_system.wordpress-efs.id}"
-  security_groups = "${aws_security_group.wordpress-efs-sg.id}"
-  subnet_id       = "${aws_subnet.public-c.id}"
+  file_system_id = aws_efs_file_system.wordpress-efs.id
+  subnet_id      = aws_subnet.public-c.id
 }
 
 # ecs
 resource "aws_ecs_task_definition" "wordpress-task-def" {
-  family = "wordpress"
+  family                   = "wordpress"
   requires_compatibilities = ["FARGATE"]
-  cpu    = "512"
-  memory = "2048"
-  network_mode = "awsvpc"
+  cpu                      = "512"
+  memory                   = "2048"
+  network_mode             = "awsvpc"
 
-  container_definitions = <<EOL
-[
-  {
-    "command":[],
-    "cpu":384,
-    "environment":[
-      {"name":"DB_HOST","value":"${var.db_endpoint}"},
-      {"name":"DB_PASSWORD","value":"${var.db_password}"}
-    ],
-    "essential":true,
-    "healthCheck":{
-      "command":["CMD-SHELL","curl -f http://localhost:9000/ || exit 0"],
-      "interval":30,
-      "retries":3,
-      "startPeriod":3,
-      "timeout":5
+  container_definitions = jsonencode([
+    {
+      "command" : [],
+      "cpu" : 384,
+      "environment" : [
+        { "name" : "DB_HOST", "value" : "${var.db_endpoint}" },
+        { "name" : "DB_PASSWORD", "value" : "${var.db_password}" }
+      ],
+      "essential" : true,
+      "healthCheck" : {
+        "command" : ["CMD-SHELL", "curl -f http://localhost:9000/ || exit 0"],
+        "interval" : 30,
+        "retries" : 3,
+        "startPeriod" : 3,
+        "timeout" : 5
+      },
+      "image" : "${data.aws_caller_identity.self.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_app:latest",
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : { "awslogs-group" : "/ecs/wordpress", "awslogs-region" : "ap-northeast-1", "awslogs-stream-prefix" : "ecs" }
+      },
+      "memoryReservation" : 1536,
+      "mountPoints" : [{ "containerPath" : "/var/www/html/", "sourceVolume" : "wordpress" }],
+      "name" : "app",
+      "portMappings" : [{ "containerPort" : 9000, "hostPort" : 9000, "protocol" : "tcp" }],
+      "volumesFrom" : []
     },
-    "image":"${var.ecr_app_image}:latest",
-    "logConfiguration":{
-      "logDriver":"awslogs",
-      "options":{"awslogs-group":"/ecs/wordpress","awslogs-region":"ap-northeast-1","awslogs-stream-prefix":"ecs"}
-    },
-    "memoryReservation":1536,
-    "mountPoints":[{"containerPath":"/var/www/html/","sourceVolume":"wordpress"}],
-    "name":"app",
-    "portMappings":[{"containerPort":9000,"hostPort":9000,"protocol":"tcp"}],
-    "volumesFrom":[]
-  },
-  {
-    "command":["envsubst '$$APP_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf \u0026\u0026 nginx -g 'daemon off;'"],
-    "cpu":128,
-    "dependsOn":[{"condition":"START","containerName":"app"}],
-    "entryPoint":["sh","-c"],
-    "environment":[{"name":"APP_HOST","value":"localhost"}],
-    "essential":true,
-    "healthCheck":{
-      "command":["CMD-SHELL","curl -f http://localhost:80/healthcheck/|| exit 1"],
-      "interval":30,
-      "retries":3,
-      "startPeriod":3,
-      "timeout":5
-    },
-    "image":"${var.ecr_web_image}:latest",
-    "links":[],
-    "logConfiguration":{
-      "logDriver":"awslogs",
-      "options":{
-        "awslogs-group":"/ecs/wordpress",
-        "awslogs-region":"ap-northeast-1",
-        "awslogs-stream-prefix":"ecs"
-      }
-    },
-    "memoryReservation":512,
-    "mountPoints":[{"containerPath":"/var/www/html/","sourceVolume":"wordpress"}],
-    "name":"web",
-    "portMappings":[{"containerPort":80,"hostPort":80,"protocol":"tcp"}],
-    "volumesFrom":[]
-  }
-]
-EOL
+    {
+      "command" : ["envsubst '$$APP_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf \u0026\u0026 nginx -g 'daemon off;'"],
+      "cpu" : 128,
+      "dependsOn" : [{ "condition" : "START", "containerName" : "app" }],
+      "entryPoint" : ["sh", "-c"],
+      "environment" : [{ "name" : "APP_HOST", "value" : "localhost" }],
+      "essential" : true,
+      "healthCheck" : {
+        "command" : ["CMD-SHELL", "curl -f http://localhost:80/healthcheck/|| exit 1"],
+        "interval" : 30,
+        "retries" : 3,
+        "startPeriod" : 3,
+        "timeout" : 5
+      },
+      "image" : "${data.aws_caller_identity.self.account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_web:latest",
+      "links" : [],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-group" : "/ecs/wordpress",
+          "awslogs-region" : "ap-northeast-1",
+          "awslogs-stream-prefix" : "ecs"
+        }
+      },
+      "memoryReservation" : 512,
+      "mountPoints" : [{ "containerPath" : "/var/www/html/", "sourceVolume" : "wordpress" }],
+      "name" : "web",
+      "portMappings" : [{ "containerPort" : 80, "hostPort" : 80, "protocol" : "tcp" }],
+      "volumesFrom" : []
+    }
+  ])
 
   volume {
     efs_volume_configuration {
@@ -298,7 +294,7 @@ EOL
         iam = "DISABLED"
       }
 
-      file_system_id          = "${aws_efs_file_system.wordpress-efs.id}"
+      file_system_id          = aws_efs_file_system.wordpress-efs.id
       root_directory          = "/"
       transit_encryption      = "DISABLED"
       transit_encryption_port = "0"
@@ -317,12 +313,10 @@ resource "aws_ecs_cluster" "wordpress-cluster" {
   }
 }
 resource "aws_ecs_service" "wordpress-service" {
-  cluster = "wordpress"
-
-  deployment_controller {
-    type = "ECS"
-  }
-
+  cluster                            = "wordpress"
+  name                               = "wordpress"
+  platform_version                   = "1.4.0"
+  scheduling_strategy                = "REPLICA"
   deployment_maximum_percent         = "200"
   deployment_minimum_healthy_percent = "100"
   desired_count                      = "2"
@@ -330,19 +324,16 @@ resource "aws_ecs_service" "wordpress-service" {
   enable_execute_command             = "true"
   health_check_grace_period_seconds  = "30"
   launch_type                        = "FARGATE"
+  task_definition                    = aws_ecs_task_definition.wordpress-task-def.arn
 
   load_balancer {
     container_name   = "web"
     container_port   = "80"
-    target_group_arn = "${aws_lb_target_group.wordpress-tgg.arn}"
+    target_group_arn = aws_lb_target_group.wordpress-tgg.arn
   }
-  name = "wordpress"
   network_configuration {
     assign_public_ip = "true"
     security_groups  = ["${aws_security_group.wordpress-service-sg.id}"]
     subnets          = ["${aws_subnet.public-a.id}", "${aws_subnet.public-c.id}"]
   }
-  platform_version    = "1.4.0"
-  scheduling_strategy = "REPLICA"
-  task_definition     = "${aws_ecs_task_definition.wordpress-task-def.arn}"
 }

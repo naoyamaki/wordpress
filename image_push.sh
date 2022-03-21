0ff9c7ab5f935684169001228cf7ac8098f5845c
naoyamaki <stringbassandsticks@gmail.com>
2022-03-21 20:40:04 +0900

    ややリファクタリング
    ・イメージをECRにpushするシェルにコメント追加
    ・Webサーバの環境変数で置換をするコマンドをdockerfileに移動
    ・wordpressイメージを基にしてもwp-content配下をマウントできればOKと考え一案として追加

--- image_push.sh
+++ image_push.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# AWSのアカウントIDをawscliから取得（複数アカウントを接している場合は注意）
 AWS_ACOUNT_ID=`aws sts get-caller-identity --query Account --output text`
-docker-compose down --rmi all --volumes --remove-orphans
+
+# 一度composeで作成した資材を削除してからイメージを再作成
+docker compose down --rmi all --volumes --remove-orphans
 docker compose build
+
+# AWS ECRへログイン
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin https://${AWS_ACOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
+
+# webサーバのイメージを作成してECRへプッシュ
 docker tag wp_web:latest ${AWS_ACOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_web:latest
 docker push ${AWS_ACOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_web:latest
+
+# appサーバのイメージを作成してECRへプッシュ
 docker tag wp_app:latest ${AWS_ACOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_app:latest
 docker push ${AWS_ACOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/wp_app:latest
